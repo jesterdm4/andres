6bb64cc3051e7bff93577990f1a5fefbbf839da0
Jose <lunaxp5@gmail.com>
2022-06-07 00:08:10 -0400

    logo listo, siguiente paso es crear el contador

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import logo from "./imagenes/logo.png";
 
 function App() {
   return (
     <div className="App">
-
+        <div className="logo-contenedor">
+            <img
+              src={logo} 
+              className="logo" 
+              alt="logo" 
+            />
+        </div>
+        <div className="contenedor-principal">
+        
+        </div>
     </div>
   );
 }
