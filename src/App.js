4182c99ee6fea1516fbdcb22b01ec2d19d0eb360
Jose <lunaxp5@gmail.com>
2022-06-08 19:22:42 -0400

    contador terminado, solo faltan dudas

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import './App.css';
 import logo from "./imagenes/logo.png";
+import Boton from "./componentes/Boton.jsx";
+import "./hojas-de-estilo/Boton.css";
+import Contador from "./componentes/Contador.jsx";
+import "./hojas-de-estilo/Contador.css";
+import { useState } from "react";
 
 function App() {
-  return (
+
+  //-------------------------------------------------------------------------------------------------------------------------------------------
+
+  //duda numero 1 del bloc de notas de preguntas - ¿las funciones van aqui y luego se asignan o van en las funciones prop de los componentes?
+    /*ejemplo de la funcion que se pondria para llenar los props de los botones
+    
+    const manejarClic = () => {
+       console.log("Click!");
+    }
+
+    para luego llenar el prop de la funcion con:
+
+    manejarClic={() => {manejarClic} 
+    
+    -------------------------------------------------------------------------------------------------------------------------------------------
+    */
+
+    const [numClics, setNumClics] = useState(0);
+
+    return (
     <div className="App">
         <div className="logo-contenedor">
             <img
@@ -11,8 +35,31 @@ function App() {
               alt="logo" 
             />
         </div>
-        <div className="contenedor-principal">
-        
+
+        <div className='contenedor-contador'>
+          <Contador //duda, donde se actualiza este numClics, porque en el boton solo se le suma 1 a la variable, pero una vez sumada no se almacena en ningina parte y menos en si misma
+            numClics={numClics} 
+          />
+        </div>
+
+        <div className="contenedor-principal" id="contenedor-principal">
+
+          <Boton //duda numero 1 del bloc de notas de preguntas - este es el boton cuya funcion tiene un prop funcion, ¿se debe llenar aqui la funcion o es mejor arriba?
+            texto="Clic" 
+            esBotonDeClic={true} 
+            manejarClic={() => {
+              setNumClics(numClics + 1);
+            }} 
+          />
+
+          <Boton //duda numero 1 del bloc de notas de preguntas - este es el boton cuya funcion tiene un prop funcion, ¿se debe llenar aqui la funcion o es mejor arriba?
+            texto="Reiniciar" 
+            esBotonDeClic={false} 
+            manejarClic={() => {
+              setNumClics(0);
+            }} 
+          />
+
         </div>
     </div>
   );
---
+++ src/componentes/Contador.jsx
@@ -0,0 +1,9 @@
+const Contador = (props) => {
+    return (
+        <div className="contador">
+        <p>{props.numClics}</p>
+        </div>
+    );
+}
+
+export default Contador;
